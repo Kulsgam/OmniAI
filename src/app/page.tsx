1ac9cf53bd9a85b97b1d42a3eb49b856a3faa003
PewterZz <peterns1609@gmail.com>
2024-12-08 17:22:40 +1100

    added video api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
+import gen from "./api/ai/video/videoGenerator";
 
 export default function Home() {
   return (
     <div className="bg-red-600">
-      <Button className="bg-black text-white hover:bg-gray-800">
+      <Button className="bg-black text-white hover:bg-gray-800" onClick={() => gen("Peter", "Peter")}>
         Click me
       </Button>
     </div>
