1e6ef1fe4f9022ee896e76e87b688885a61473fa
VimHax <vimukthi.weerabahu@gmail.com>
2024-12-10 05:18:50 +0530

    Remove logs

--- src/app/api/ai/image/route.ts
+++ src/app/api/ai/image/route.ts
@@ -51,8 +51,6 @@ export async function GET(request: Request) {
     aspectRatio !== "landscape" &&
     aspectRatio !== "square"
   ) {
-    console.log("x");
-
     return new Response("Invalid input.", { status: 400 });
   }
 
